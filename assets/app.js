1cf2a907b09f2e3896abbcd551f95b85be2e494e
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2023-08-02 23:28:18 +0100

    mario restart

--- assets/app.js
+++ assets/app.js
@@ -75,6 +75,6 @@ const checkGameOver = setInterval(() => {
 		setTimeout(() => {
 			alert("Game Over, sua pontuação foi de " + count);
 			window.location.reload();
-		}, 11);
+		}, 1000);
 	}
 }, 10);
